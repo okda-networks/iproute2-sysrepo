1ebcac2d73d8e7a9f070de45513de8a8b4ac9997
Ali Aqrabawi <aaqrabaw@okdanetworks.com>
2024-02-24 00:13:46 +0300

    cmdgen, test: add arg-name support and doc strings, added nethop tests (#17)
    * cmdgen, test: add arg-name support and doc strings, added nethop tests

--- src/lib/cmdgen.h
+++ src/lib/cmdgen.h
@@ -7,6 +7,11 @@
 #define CMDS_ARRAY_SIZE 1024
 
 /**
+ * extension to extension name map.
+ */
+extern char *yang_ext_map[];
+
+/**
  * @brief data struct to store cmd operation.
  *
  */
@@ -16,6 +21,26 @@ typedef enum {
     UPDATE_OPR,
     UNKNOWN_OPR,
 } oper_t;
+
+/**
+ * definition of yang's extension_t.
+ */
+typedef enum {
+    // list extensions
+    CMD_START_EXT,
+    // root container extensions
+    CMD_ADD_EXT,
+    CMD_DELETE_EXT,
+    CMD_UPDATE_EXT,
+
+    // leaf extensions
+    ARG_NAME_EXT,
+    FLAG_EXT,
+    VALUE_ONLY_EXT,
+    VALUE_ONLY_ON_UPDATE_EXT
+
+
+} extension_t;
 
 /**
  * @brief data struct to store command information.
@@ -46,12 +71,19 @@ struct cmd_info** lyd2cmds(const struct lyd_node *change_node);
 struct cmd_info* lyd2rollback_cmd(const struct lyd_node *change_node);
 
 /**
- * @brief get the change operation from change lyd_node.
- *
- *
- * @param[in] lyd_node change node.
- * @return oper_t
+ * get the yang operation from the lyd_node (create, replace, delete)
+ * @param [in]dnode lyd_node
+ * @return oper_t the time of the operation found on this lyd_node.
  */
 oper_t get_operation(const struct lyd_node *dnode);
+
+/**
+ * get the extension from lyd_node,
+ * @param [in] ex_t extension_t to be captured from lyd_node.
+ * @param [in] dnode lyd_node where to search for provided ext.
+ * @param [out] value extension value if found. can be null.
+ * @return EXIT_SUCCESS if ext found, EXIT_FAILURE if not found
+ */
+int get_extension(extension_t ex_t, const struct lyd_node *dnode, char **value);
 
 #endif// IPROUTE2_SYSREPO_CMDGEN_H
