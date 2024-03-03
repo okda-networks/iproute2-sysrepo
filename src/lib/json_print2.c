2487211979fda73de58a9fb4b474b76e618a4ce5
Vincent JARDIN <vjardin@free.fr>
2024-03-03 12:53:05 +0100

    clang-format: format lib/json_print2.c
    The code was derived from iproute2, but let's align it with the local
    clang-format.

--- src/lib/json_print2.c
+++ src/lib/json_print2.c
@@ -17,47 +17,46 @@ static json_writer_t *_jw;
 
 static void __new_json_obj_mem(int json, bool have_array)
 {
-	if (json) {
-		j_stream = fmemopen(json_buffer, sizeof(json_buffer), "w");
-		_jw = jsonw_new(j_stream);
-		if (!_jw) {
-			perror("json object");
-			exit(1);
-		}
-		if (pretty)
-			jsonw_pretty(_jw, true);
-		if (have_array)
-			jsonw_start_array(_jw);
-	}
+    if (json) {
+        j_stream = fmemopen(json_buffer, sizeof(json_buffer), "w");
+        _jw = jsonw_new(j_stream);
+        if (!_jw) {
+            perror("json object");
+            exit(1);
+        }
+        if (pretty)
+            jsonw_pretty(_jw, true);
+        if (have_array)
+            jsonw_start_array(_jw);
+    }
 }
 
 static void __delete_json_obj_mem(bool have_array)
 {
-	if (_jw) {
-		if (have_array)
-			jsonw_end_array(_jw);
-		jsonw_destroy(&_jw);
-		fclose(j_stream);
-	}
+    if (_jw) {
+        if (have_array)
+            jsonw_end_array(_jw);
+        jsonw_destroy(&_jw);
+        fclose(j_stream);
+    }
 }
 
 void new_json_obj(int json)
 {
-	__new_json_obj_mem(json, true);
+    __new_json_obj_mem(json, true);
 }
 
 void delete_json_obj(void)
 {
-	__delete_json_obj_mem(true);
+    __delete_json_obj_mem(true);
 }
 
 void new_json_obj_plain(int json)
 {
-	__new_json_obj_mem(json, false);
+    __new_json_obj_mem(json, false);
 }
 
 void delete_json_obj_plain(void)
 {
-	__delete_json_obj_mem(false);
+    __delete_json_obj_mem(false);
 }
-
